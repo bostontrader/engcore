077f2b274d26770b236308fa6721b0c7148b563d
Thomas Radloff <bostontrader@gmail.com>
2017-07-04 16:26:27 +0800

    Fix index.js

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,37 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Plurality = exports.NounOb = exports.NGenerateText = undefined;
+exports.VPOb = exports.VPGenerateText = exports.VGenerateText = exports.VDictOb = exports.ProOb = exports.ProGenerateText = exports.NPOb = exports.NPGenerateText = exports.Plurality = exports.NounOb = exports.NGenerateText = exports.NDictOb = exports.AdjDictOb = exports.AdjGenerateText = undefined;
+
+var _Adjective = require('./Adjective');
+
+var _AdjDictionary = require('./AdjDictionary');
+
+var _NDictionary = require('./NDictionary');
 
 var _Noun = require('./Noun');
 
+var _NP = require('./NP');
+
+var _Pronoun = require('./Pronoun');
+
+var _VDictionary = require('./VDictionary');
+
+var _Verb = require('./Verb');
+
+var _VP = require('./VP');
+
+exports.AdjGenerateText = _Adjective.AdjGenerateText;
+exports.AdjDictOb = _AdjDictionary.AdjDictOb;
+exports.NDictOb = _NDictionary.NDictOb;
 exports.NGenerateText = _Noun.NGenerateText;
 exports.NounOb = _Noun.NounOb;
 exports.Plurality = _Noun.Plurality;
+exports.NPGenerateText = _NP.NPGenerateText;
+exports.NPOb = _NP.NPOb;
+exports.ProGenerateText = _Pronoun.ProGenerateText;
+exports.ProOb = _Pronoun.ProOb;
+exports.VDictOb = _VDictionary.VDictOb;
+exports.VGenerateText = _Verb.VGenerateText;
+exports.VPGenerateText = _VP.VPGenerateText;
+exports.VPOb = _VP.VPOb;
