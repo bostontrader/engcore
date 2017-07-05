7a820d413d8aacc72dfb5489f9fe9e88286d4842
Thomas Radloff <bostontrader@gmail.com>
2017-07-05 14:43:45 +0800

    Add many words to the dictionaries.

--- src/AdjDictionary.js
+++ src/AdjDictionary.js
@@ -8,13 +8,19 @@ const AdjDictOb = {
     green:  new AdjOb({base:'green'}),
     honest: new AdjOb({base:'honest', silentH: true}),
     itchy:  new AdjOb({base:'itchy'}),
+    kite:  new AdjOb({base:'kite'}),
     long:   new AdjOb({base:'long'}),
     lonely: new AdjOb({base:'lonely'}),
+    little: new AdjOb({base:'little'}),
+    new: new AdjOb({base:'new'}),
+
     red:    new AdjOb({base:'red'}),
     small:  new AdjOb({base:'small'}),
     tiny:   new AdjOb({base:'tiny'}),
+    toy:   new AdjOb({base:'toy'}),
     ugly:   new AdjOb({base:'ugly'}),
     wet:    new AdjOb({base:'wet'}),
+    wood:    new AdjOb({base:'wood'}),
 }
 
 export {AdjDictOb}
