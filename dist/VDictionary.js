8d492eb66986b377b089eb00c9db0d83cef2b8f0
Thomas Radloff <bostontrader@gmail.com>
2017-07-05 15:13:22 +0800

    Add some words to the dictionaries.

--- dist/VDictionary.js
+++ dist/VDictionary.js
@@ -11,15 +11,17 @@ var VDictOb = {
     //be:  new VerbOb({base:'be',  pastForm:'did'}),
     do: new _Verb.VerbOb({ base: 'do', pastForm: 'did' }),
     fall: new _Verb.VerbOb({ base: 'fall', pastForm: 'fell' }),
+    find: new _Verb.VerbOb({ base: 'find', pastForm: 'found' }),
     fly: new _Verb.VerbOb({ base: 'fly', pastForm: 'flew' }),
     go: new _Verb.VerbOb({ base: 'go', pastForm: 'went' }),
     have: new _Verb.VerbOb({ base: 'have', pastForm: 'had' }),
     know: new _Verb.VerbOb({ base: 'know', pastForm: 'knew' }),
     play: new _Verb.VerbOb({ base: 'play', pastForm: 'played' }),
+    say: new _Verb.VerbOb({ base: 'say', pastForm: 'said' }),
     shake: new _Verb.VerbOb({ base: 'shake', pastForm: 'shook' }),
     shine: new _Verb.VerbOb({ base: 'shine', pastForm: 'shined' }),
     sit: new _Verb.VerbOb({ base: 'sit', pastForm: 'sat' }),
-    say: new _Verb.VerbOb({ base: 'say', pastForm: 'said' })
+    stop: new _Verb.VerbOb({ base: 'stop', pastForm: 'stopped' })
 };
 
 exports.VDictOb = VDictOb;
--- src/VDictionary.js
+++ src/VDictionary.js
@@ -4,15 +4,17 @@ const VDictOb = {
     //be:  new VerbOb({base:'be',  pastForm:'did'}),
     do:    new VerbOb({base:'do',    pastForm:'did'}),
     fall:  new VerbOb({base:'fall',  pastForm:'fell'}),
+    find:  new VerbOb({base:'find',  pastForm:'found'}),
     fly:   new VerbOb({base:'fly',   pastForm:'flew'}),
     go:    new VerbOb({base:'go',    pastForm:'went'}),
     have:  new VerbOb({base:'have',  pastForm:'had'}),
     know:  new VerbOb({base:'know',  pastForm:'knew'}),
     play:  new VerbOb({base:'play',  pastForm:'played'}),
+    say:   new VerbOb({base:'say',   pastForm:'said'}),
     shake: new VerbOb({base:'shake', pastForm:'shook'}),
     shine: new VerbOb({base:'shine', pastForm:'shined'}),
     sit:   new VerbOb({base:'sit',   pastForm:'sat'}),
-    say:   new VerbOb({base:'say',   pastForm:'said'}),
+    stop:  new VerbOb({base:'stop',  pastForm:'stopped'}),
 }
 
 export {VDictOb}
