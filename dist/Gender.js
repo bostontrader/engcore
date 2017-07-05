72eabdf1f969378c85a923c55fa35a7060f815bd
Thomas Radloff <bostontrader@gmail.com>
2017-07-05 15:44:39 +0800

    Implement pronouns "you", "he", and "she".

---
+++ dist/Gender.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var Gender = {
+    'NoneSelected': 0,
+    'Male': 100,
+    'Female': 200
+};
+
+exports.default = Gender;
