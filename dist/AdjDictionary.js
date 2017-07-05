0674bd31ab9ebbc90e702f825fb16834b4a9eb22
Thomas Radloff <bostontrader@gmail.com>
2017-07-05 14:48:38 +0800

    Add many words to the dictionaries.

--- dist/AdjDictionary.js
+++ dist/AdjDictionary.js
@@ -15,13 +15,19 @@ var AdjDictOb = {
     green: new _Adjective.AdjOb({ base: 'green' }),
     honest: new _Adjective.AdjOb({ base: 'honest', silentH: true }),
     itchy: new _Adjective.AdjOb({ base: 'itchy' }),
+    kite: new _Adjective.AdjOb({ base: 'kite' }),
     long: new _Adjective.AdjOb({ base: 'long' }),
     lonely: new _Adjective.AdjOb({ base: 'lonely' }),
+    little: new _Adjective.AdjOb({ base: 'little' }),
+    new: new _Adjective.AdjOb({ base: 'new' }),
+
     red: new _Adjective.AdjOb({ base: 'red' }),
     small: new _Adjective.AdjOb({ base: 'small' }),
     tiny: new _Adjective.AdjOb({ base: 'tiny' }),
+    toy: new _Adjective.AdjOb({ base: 'toy' }),
     ugly: new _Adjective.AdjOb({ base: 'ugly' }),
-    wet: new _Adjective.AdjOb({ base: 'wet' })
+    wet: new _Adjective.AdjOb({ base: 'wet' }),
+    wood: new _Adjective.AdjOb({ base: 'wood' })
 };
 
 exports.AdjDictOb = AdjDictOb;
--- dist/VDictionary.js
+++ dist/VDictionary.js
@@ -9,9 +9,19 @@ var _Verb = require('./Verb');
 
 // The dictionary should be immutable
 var VDictOb = {
+    //be:  new VerbOb({base:'be',  pastForm:'did'}),
+    do: new _Verb.VerbOb({ base: 'do', pastForm: 'did' }),
+    fall: new _Verb.VerbOb({ base: 'fall', pastForm: 'fell' }),
+    go: new _Verb.VerbOb({ base: 'go', pastForm: 'went' }),
+    have: new _Verb.VerbOb({ base: 'have', pastForm: 'had' }),
+    know: new _Verb.VerbOb({ base: 'know', pastForm: 'knew' }),
+
     play: new _Verb.VerbOb({ base: 'play', pastForm: 'played' }),
     shine: new _Verb.VerbOb({ base: 'shine', pastForm: 'shined' }),
-    sit: new _Verb.VerbOb({ base: 'sit', pastForm: 'sat' })
+    sit: new _Verb.VerbOb({ base: 'sit', pastForm: 'sat' }),
+
+    say: new _Verb.VerbOb({ base: 'say', pastForm: 'said' })
+
 };
 
 exports.VDictOb = VDictOb;
