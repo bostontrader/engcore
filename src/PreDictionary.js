3eec510bfa47d5b17bb925c8267f9776977090e3
Thomas Radloff <bostontrader@gmail.com>
2017-07-07 16:57:23 +0800

    Implement prepositional phrase.

--- src/PreDictionary.js
+++ src/PreDictionary.js
@@ -1,8 +1,12 @@
 import {PreOb} from './Pre'
 
 const PreDictOb = {
-    in: new PreOb({base:'in'}),
-    on: new PreOb({base:'on'}),
+    in:   new PreOb({base:'in'}),
+    into: new PreOb({base:'into'}),
+    of:   new PreOb({base:'of'}),
+    on:   new PreOb({base:'on'}),
+    to:   new PreOb({base:'to'}),
+    with: new PreOb({base:'with'}),
 }
 
 export {PreDictOb}
