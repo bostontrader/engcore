19da9853c4a3de212e295432e11d150046799d99
Thomas Radloff <bostontrader@gmail.com>
2017-07-09 12:16:34 +0800

    An empty Pronoun should default to something sensible.  How about "I"?

--- dist/Pronoun.js
+++ dist/Pronoun.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 exports.PronounType = exports.ProGenerateText = exports.ProOb = undefined;
 
@@ -18,19 +18,19 @@ var _Noun = require('./Noun');
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function ProOb(props) {
-    this.t = _SchemaConstants2.default.Pro.t;
-    this.v = _SchemaConstants2.default.Pro.cv;
-    this.type = props.type || PronounType.Subject;
-    this.plurality = props.plurality || _Noun.Plurality.NoneSelected;
-    this.person = props.person || 1;
-    this.gender = props.gender || _Gender2.default.NoneSelected;
+	this.t = _SchemaConstants2.default.Pro.t;
+	this.v = _SchemaConstants2.default.Pro.cv;
+	this.type = props.type || PronounType.Subject;
+	this.plurality = props.plurality || _Noun.Plurality.Singular;
+	this.person = props.person || 1;
+	this.gender = props.gender || _Gender2.default.NoneSelected;
 }
 
 var PronounType = {
-    'Subject': 0,
-    'Object': 1,
-    'Possessive': 2,
-    'ReflexiveIntensive': 3
+	'Subject': 0,
+	'Object': 1,
+	'Possessive': 2,
+	'ReflexiveIntensive': 3
 };
 
 var PronounTable = [[['I', 'we'], ['you', 'you'], [['he', 'she', 'it'], 'they']], // subject
@@ -40,30 +40,30 @@ var PronounTable = [[['I', 'we'], ['you', 'you'], [['he', 'she', 'it'], 'they']]
 
 var ProGenerateText = function ProGenerateText(pronoun) {
 
-    var retVal = 'BAD PRONOUN CONFIGURATION';
+	var retVal = 'BAD PRONOUN CONFIGURATION';
 
-    var type = pronoun.type,
-        plurality = pronoun.plurality,
-        person = pronoun.person,
-        gender = pronoun.gender;
+	var type = pronoun.type,
+	    plurality = pronoun.plurality,
+	    person = pronoun.person,
+	    gender = pronoun.gender;
 
-    // Convert constant values to indices for use here.
+	// Convert constant values to indices for use here.
 
-    var type_idx = type;
-    var plurality_idx = plurality === _Noun.Plurality.Singular ? 0 : 1;
-    var person_idx = person - 1;
+	var type_idx = type;
+	var plurality_idx = plurality === _Noun.Plurality.Singular ? 0 : 1;
+	var person_idx = person - 1;
 
-    retVal = PronounTable[type_idx][person_idx][plurality_idx];
+	retVal = PronounTable[type_idx][person_idx][plurality_idx];
 
-    if (person === 3 && plurality === _Noun.Plurality.Singular) if (gender === _Gender2.default.Male) {
-        retVal = retVal[0];
-    } else if (gender === _Gender2.default.Female) {
-        retVal = retVal[1];
-    } else {
-        retVal = retVal[2];
-    }
+	if (person === 3 && plurality === _Noun.Plurality.Singular) if (gender === _Gender2.default.Male) {
+		retVal = retVal[0];
+	} else if (gender === _Gender2.default.Female) {
+		retVal = retVal[1];
+	} else {
+		retVal = retVal[2];
+	}
 
-    return retVal;
+	return retVal;
 };
 
 exports.ProOb = ProOb;
