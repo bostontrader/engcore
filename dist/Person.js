d976cbe7dd24cef09c44ce5e546c962f5d1e87b2
Thomas Radloff <bostontrader@gmail.com>
2017-07-10 04:03:09 +0800

    Rework Person constants.

---
+++ dist/Person.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var Person = {
+	'NoneSelected': 0,
+	'First': 100,
+	'Second': 200,
+	'Third': 300
+};
+
+exports.default = Person;
