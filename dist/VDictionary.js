4abbb7624fa8a99fbc8fdbbba058aa6d7ac81b57
Thomas Radloff <bostontrader@gmail.com>
2017-07-14 14:39:57 +0800

    New tests. New words.

--- dist/VDictionary.js
+++ dist/VDictionary.js
@@ -1,34 +1,43 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 exports.VDictOb = undefined;
 
 var _Verb = require('./Verb');
 
 var VDictOb = {
-    arrive: new _Verb.VerbOb({ base: 'arrive', pastForm: 'arrived' }),
-    //be:  new VerbOb({base:'be',  pastForm:'did'}),
-    do: new _Verb.VerbOb({ base: 'do', pastForm: 'did' }),
-    fall: new _Verb.VerbOb({ base: 'fall', pastForm: 'fell' }),
-    find: new _Verb.VerbOb({ base: 'find', pastForm: 'found' }),
-    fly: new _Verb.VerbOb({ base: 'fly', pastForm: 'flew' }),
-    go: new _Verb.VerbOb({ base: 'go', pastForm: 'went' }),
-    have: new _Verb.VerbOb({ base: 'have', pastForm: 'had' }),
-    inform: new _Verb.VerbOb({ base: 'inform', pastForm: 'informed' }),
-    know: new _Verb.VerbOb({ base: 'know', pastForm: 'knew' }),
-    leave: new _Verb.VerbOb({ base: 'leave', pastForm: 'left' }),
-    look: new _Verb.VerbOb({ base: 'look', pastForm: 'looked' }),
-    play: new _Verb.VerbOb({ base: 'play', pastForm: 'played' }),
-    say: new _Verb.VerbOb({ base: 'say', pastForm: 'said' }),
-    shake: new _Verb.VerbOb({ base: 'shake', pastForm: 'shook' }),
-    shine: new _Verb.VerbOb({ base: 'shine', pastForm: 'shined' }),
-    show: new _Verb.VerbOb({ base: 'show', pastForm: 'showed' }),
-    sit: new _Verb.VerbOb({ base: 'sit', pastForm: 'sat' }),
-    steal: new _Verb.VerbOb({ base: 'steal', pastForm: 'stole' }),
-    stop: new _Verb.VerbOb({ base: 'stop', pastForm: 'stopped' }),
-    work: new _Verb.VerbOb({ base: 'work', pastForm: 'worked' })
+	arrive: new _Verb.VerbOb({ base: 'arrive', pastForm: 'arrived' }),
+	be: new _Verb.VerbOb({ base: 'be', pastForm: 'was' }),
+	do: new _Verb.VerbOb({ base: 'do', pastForm: 'did' }),
+	fall: new _Verb.VerbOb({ base: 'fall', pastForm: 'fell' }),
+	find: new _Verb.VerbOb({ base: 'find', pastForm: 'found' }),
+	fly: new _Verb.VerbOb({ base: 'fly', pastForm: 'flew' }),
+	go: new _Verb.VerbOb({ base: 'go', pastForm: 'went' }),
+	have: new _Verb.VerbOb({ base: 'have', pastForm: 'had' }),
+	inform: new _Verb.VerbOb({ base: 'inform', pastForm: 'informed' }),
+	know: new _Verb.VerbOb({ base: 'know', pastForm: 'knew' }),
+	leave: new _Verb.VerbOb({ base: 'leave', pastForm: 'left' }),
+	look: new _Verb.VerbOb({ base: 'look', pastForm: 'looked' }),
+	place: new _Verb.VerbOb({ base: 'place', pastForm: 'placed' }),
+	play: new _Verb.VerbOb({ base: 'play', pastForm: 'played' }),
+	run: new _Verb.VerbOb({ base: 'run', pastForm: 'ran' }),
+	say: new _Verb.VerbOb({ base: 'say', pastForm: 'said' }),
+	see: new _Verb.VerbOb({ base: 'see', pastForm: 'saw' }),
+	shake: new _Verb.VerbOb({ base: 'shake', pastForm: 'shook' }),
+	shine: new _Verb.VerbOb({ base: 'shine', pastForm: 'shined' }),
+	show: new _Verb.VerbOb({ base: 'show', pastForm: 'showed' }),
+	sigh: new _Verb.VerbOb({ base: 'sigh', pastForm: 'sighed' }),
+	sit: new _Verb.VerbOb({ base: 'sit', pastForm: 'sat' }),
+	steal: new _Verb.VerbOb({ base: 'steal', pastForm: 'stole' }),
+	stop: new _Verb.VerbOb({ base: 'stop', pastForm: 'stopped' }),
+	take: new _Verb.VerbOb({ base: 'take', pastForm: 'took' }),
+	turn: new _Verb.VerbOb({ base: 'turn', pastForm: 'turned' }),
+	visit: new _Verb.VerbOb({ base: 'visit', pastForm: 'visited' }),
+	walk: new _Verb.VerbOb({ base: 'walk', pastForm: 'walked' }),
+	wash: new _Verb.VerbOb({ base: 'wash', pastForm: 'washed' }),
+	work: new _Verb.VerbOb({ base: 'work', pastForm: 'worked' })
 };
 
 exports.VDictOb = VDictOb;
--- dist/VP.js
+++ dist/VP.js
@@ -1,11 +1,15 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 exports.VPOb = exports.VPGenerateText = undefined;
 
 var _Noun = require('./Noun');
+
+var _Person = require('./Person');
+
+var _Person2 = _interopRequireDefault(_Person);
 
 var _SchemaConstants = require('./SchemaConstants');
 
@@ -17,139 +21,165 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function VPOb(props) {
 
-    this.t = _SchemaConstants2.default.VP.t;
-    this.v = _SchemaConstants2.default.VP.cv;
+	this.t = _SchemaConstants2.default.VP.t;
+	this.v = _SchemaConstants2.default.VP.cv;
 
-    this.verb = props.verb;
+	this.verb = props.verb;
 
-    // Given all these settings, a determined adversary can probably be combine them into non-sensensical patterns.
-    // Instead of fretting about this possibility, a VP will merely operate upon the first sensible pattern that it 
-    // recognizes.  Please see VPGenerateText to understand the exact method of recognition.
+	// Given all these settings, a determined adversary can probably combine them into non-sensensical patterns.
+	// Instead of fretting about this possibility, a VP will merely operate upon the first sensible pattern that it
+	// recognizes.  Please see VPGenerateText to understand the exact method of recognition.
 
-    this.perfect = props.perfect;
-    this.continuous = props.continous;
-    this.tense = props.tense;
-    this.person = props.person;
-    this.passive = props.passive || false;
-    this.infinitive = props.infinitive || false;
+	this.perfect = props.perfect || false;
+	this.continuous = props.continuous || false;
+	this.tense = props.tense;
+	this.person = props.person;
+	this.passive = props.passive || false;
 
-    // A finite verb phrase is one that can be the main verb of a sentence. A non-finite
-    // verb is an infinitive, gerund or participle.
-    //this.finite = props.finite
+	// A finite verb phrase is one that can be the main verb of a sentence. A non-finite
+	// verb is an infinitive, gerund or participle.
+	this.finite = props.finite || true;
+	this.infinitive = props.infinitive || false;
 }
 
 var VPGenerateText = function VPGenerateText(vp) {
 
-    var getPastForm = function getPastForm(verbob) {
-        return verbob.pastForm ? verbob.pastForm : verbob.base + 'ed';
-    };
+	var getPastForm = function getPastForm(verbob) {
+		return verbob.pastForm ? verbob.pastForm : verbob.base + 'ed';
+	};
 
-    if (vp.perfect && vp.continuous) {
-        switch (vp.tense) {
-            case _Verb.Tense.Past:
-                if (vp.person === 1) return 'had been ' + vp.verb.base + 'ing';
-                //if(vp.person === 2) return 'were ' + vp.verb.base + 'ed'
-                //if(vp.person === 3) return 'has been ' + vp.verb.base + 'ing'
-                break;
+	if (vp.perfect && vp.continuous) {}
+	//switch(vp.tense) {
+	//case Tense.Past:
+	//if(vp.person === Person.First) return 'had been ' + vp.verb.base + 'ing'
+	//if(vp.person === Person.Second) return 'were ' + vp.verb.base + 'ed'
+	//if(vp.person === Person.Third) return 'has been ' + vp.verb.base + 'ing'
+	//break
 
-            case _Verb.Tense.Present:
-                //if(vp.person === 1) return 'have ' + getPastForm(vp.verb)
-                //if(vp.person === 2) return 'were ' + vp.verb.base + 'ed'
-                if (vp.person === 3) return 'has been ' + vp.verb.base + 'ing';
-                break;
+	//case Tense.Present:
+	//if(vp.person === Person.First) return 'have ' + getPastForm(vp.verb)
+	//if(vp.person === Person.Second) return 'were ' + vp.verb.base + 'ed'
+	//if(vp.person === Person.Third) return 'has been ' + vp.verb.base + 'ing'
+	//break
 
-            default:
-                if (vp.future) return 'will have been ' + vp.verb.base + 'ing';
+	//default:
+	//if (vp.future) return 'will have been ' + vp.verb.base + 'ing'
 
-        }
-    } else if (vp.perfect) {
+	//}
 
-        if (vp.passive) {
-            switch (vp.tense) {
-                case _Verb.Tense.Past:
-                    return 'had been ' + getPastForm(vp.verb);
-                case _Verb.Tense.Present:
-                    return 'has been ' + getPastForm(vp.verb);
-                //default: // No tense, future
-                //return 'will be being ' + getPastForm(vp.verb)
-            }
-        }
 
-        switch (vp.tense) {
-            case _Verb.Tense.Past:
-                if (vp.person === 1) return 'have ' + getPastForm(vp.verb);
-                //if(vp.person === 2) return 'were ' + vp.verb.base + 'ed'
-                if (vp.person === 3) return 'had ' + getPastForm(vp.verb);
+	//else if(vp.perfect) {
 
-            default:
-                if (vp.future) return 'will have ' + getPastForm(vp.verb);
-        }
-    } else if (vp.continuous) {
+	//if(vp.passive) {
+	//switch (vp.tense) {
+	//case Tense.Past:
+	//return 'had been ' + getPastForm(vp.verb)
+	//case Tense.Present:
+	//	return 'has been ' + getPastForm(vp.verb)
+	//default: // No tense, future
+	//return 'will be being ' + getPastForm(vp.verb)
+	//}
+	//}
 
-        if (vp.passive) {
-            switch (vp.tense) {
-                case _Verb.Tense.Past:
-                    return 'was being ' + getPastForm(vp.verb);
-                case _Verb.Tense.Present:
-                    return 'is being ' + getPastForm(vp.verb);
-                default:
-                    // No tense, future
-                    return 'will be being ' + getPastForm(vp.verb);
-            }
-        }
 
-        switch (vp.tense) {
-            case _Verb.Tense.Past:
-                if (vp.person === 1) return 'was ' + vp.verb.base + 'ing';
-                if (vp.person === 2) return 'were ' + vp.verb.base + 'ing';
-                if (vp.person === 3) {
-                    if (vp.plurality === _Noun.Plurality.Singular) return 'was ' + vp.verb.base + 'ing';else return 'were ' + vp.verb.base + 'ing';
-                }
+	//switch(vp.tense) {
+	//case Tense.Past:
+	//if(vp.person === Person.First) return 'have ' + getPastForm(vp.verb)
+	//if(vp.person === Person.Second) return 'were ' + vp.verb.base + 'ed'
+	//if(vp.person === Person.Third) return 'had ' + getPastForm(vp.verb)
 
-            case _Verb.Tense.Present:
-                var negate = vp.negate ? 'not ' : '';
-                if (vp.person === 1) if (vp.plurality === _Noun.Plurality.Singular) return 'am ' + negate + vp.verb.base + 'ing';else return 'are ' + negate + vp.verb.base + 'ing';
 
-                if (vp.person === 2) return 'are ' + negate + vp.verb.base + 'ing';
-                if (vp.person === 3) return 'is ' + negate + vp.verb.base + 'ing';
+	//default:
+	//if (vp.future) return 'will have ' + getPastForm(vp.verb)
+	//}
 
-            default:
-                if (vp.future) return 'will be ' + vp.verb.base + 'ing';
+	//}
 
-        }
-    } else {
-        // else simple
+	else if (vp.continuous) {
 
-        if (vp.infinitive) return 'to ' + vp.verb.base;
-        if (vp.passive) {
-            switch (vp.tense) {
-                case _Verb.Tense.Past:
-                    if (vp.plurality === _Noun.Plurality.Singular) return 'was ' + getPastForm(vp.verb);else return 'were ' + getPastForm(vp.verb);
-                case _Verb.Tense.Present:
-                    return 'is ' + getPastForm(vp.verb);
-                default:
-                    // No tense, future
-                    return 'will be ' + getPastForm(vp.verb);
-            }
-        }
+			//if(vp.passive) {
+			//switch (vp.tense) {
+			//case Tense.Past:
+			//return 'was being ' + getPastForm(vp.verb)
+			//case Tense.Present:
+			//return 'is being ' + getPastForm(vp.verb)
+			//default: // No tense, future
+			//return 'will be being ' + getPastForm(vp.verb)
+			//}
+			//}
 
-        var _negate = vp.negate ? 'do not ' : '';
-        switch (vp.tense) {
-            case _Verb.Tense.Past:
-                return _negate + getPastForm(vp.verb);
+			switch (vp.tense) {
+				case _Verb.Tense.Past:
+					if (vp.person === _Person2.default.First) return 'was ' + vp.verb.base + 'ing';
+					if (vp.person === _Person2.default.Second) return 'were ' + vp.verb.base + 'ing';
+				//if(vp.person === Person.Third) {
+				//if (vp.plurality === Plurality.Singular)
+				//return 'was ' + vp.verb.base + 'ing'
+				//else
+				//return 'were ' + vp.verb.base + 'ing'
+				//}
 
-            case _Verb.Tense.Present:
-                if (vp.person === 3 && vp.plurality === _Noun.Plurality.Singular) if (vp.negate) return 'does not ' + vp.verb.base;else return vp.verb.base + 's';
+				//case Tense.Present:
+				//const negate = vp.negate ? 'not ' : ''
+				//if(vp.person === Person.First)
+				//if (vp.plurality === Plurality.Singular)
+				//return 'am ' + negate + vp.verb.base + 'ing'
+				//else
+				//return 'are ' + negate + vp.verb.base + 'ing'
 
-                return _negate + vp.verb.base;
+				//if(vp.person === Person.Second) return 'are ' + negate + vp.verb.base + 'ing'
+				//if(vp.person === Person.Third) return 'is ' + negate + vp.verb.base + 'ing'
 
-            default:
-                if (vp.future) return 'will ' + vp.verb.base;
+				//default:
+				//if (vp.future) return 'will be ' + vp.verb.base + 'ing'
 
-        }
-    }
+			}
+		}
 
-    return vp.verb.base;
+		//else if(vp.infinitive) {
+		//return 'to ' + vp.verb.base
+		//}
+
+		//else if(vp.passive) {
+		//switch(vp.tense) {
+		//case Tense.Past:
+		//if (vp.plurality === Plurality.Singular)
+		//return 'was ' + getPastForm(vp.verb)
+		//else
+		//return 'were ' + getPastForm(vp.verb)
+		//case Tense.Present:
+		//return 'is ' + getPastForm(vp.verb)
+		//default: // No tense, future
+		//return 'will be ' + getPastForm(vp.verb)
+		//}
+		//}
+
+		else {
+				// else simple
+
+				//const negate = vp.negate ? 'do not ' : ''
+
+				switch (vp.tense) {
+					case _Verb.Tense.Past:
+						//return negate + getPastForm(vp.verb)
+						return getPastForm(vp.verb);
+
+					//case Tense.Present:
+					//if(vp.person === Person.Third && vp.plurality === Plurality.Singular)
+					//if (vp.negate)
+					//return 'does not ' + vp.verb.base
+					//else
+					//return vp.verb.base + 's'
+
+					//return negate + vp.verb.base
+
+					//default:
+					//if (vp.future) return 'will ' + vp.verb.base
+
+				}
+			}
+
+	return vp.verb.base;
 };
 
 exports.VPGenerateText = VPGenerateText;
