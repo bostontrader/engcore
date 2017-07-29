aedba07b72041fbc977ad33e6ec664b29376c4cb
Thomas Radloff <bostontrader@gmail.com>
2017-07-29 15:08:58 +0800

    Overhaul the Determiners.

---
+++ dist/pos/det/DetDict.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var DetDict = {
+	a: { base: 'a' },
+	an: { base: 'an' },
+	that: { base: 'that' },
+	the: { base: 'the' },
+	these: { base: 'these' },
+	this: { base: 'this' },
+	those: { base: 'those' }
+};
+
+exports.default = DetDict;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9wb3MvZGV0L0RldERpY3QuanMiXSwibmFtZXMiOlsiRGV0RGljdCIsImEiLCJiYXNlIiwiYW4iLCJ0aGF0IiwidGhlIiwidGhlc2UiLCJ0aGlzIiwidGhvc2UiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsSUFBTUEsVUFBVTtBQUNmQyxJQUFPLEVBQUNDLE1BQUssR0FBTixFQURRO0FBRWZDLEtBQU8sRUFBQ0QsTUFBSyxJQUFOLEVBRlE7QUFHZkUsT0FBTyxFQUFDRixNQUFLLE1BQU4sRUFIUTtBQUlmRyxNQUFPLEVBQUNILE1BQUssS0FBTixFQUpRO0FBS2ZJLFFBQU8sRUFBQ0osTUFBSyxPQUFOLEVBTFE7QUFNZkssT0FBTyxFQUFDTCxNQUFLLE1BQU4sRUFOUTtBQU9mTSxRQUFPLEVBQUNOLE1BQUssT0FBTjtBQVBRLENBQWhCOztrQkFVZUYsTyIsImZpbGUiOiJEZXREaWN0LmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgRGV0RGljdCA9IHtcblx0YTogICAgIHtiYXNlOidhJ30sXG5cdGFuOiAgICB7YmFzZTonYW4nfSxcblx0dGhhdDogIHtiYXNlOid0aGF0J30sXG5cdHRoZTogICB7YmFzZTondGhlJ30sXG5cdHRoZXNlOiB7YmFzZTondGhlc2UnfSxcblx0dGhpczogIHtiYXNlOid0aGlzJ30sXG5cdHRob3NlOiB7YmFzZTondGhvc2UnfVxufVxuXG5leHBvcnQgZGVmYXVsdCBEZXREaWN0XG4iXX0=
